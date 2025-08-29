ffcce1d94fc0d8aec42df9a10b9c733882c7b48d
Hoffman <Nathan.Hoffman@avl.com>
2025-08-29 12:12:22 +0200

    styling2

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://rcdbdpqnbtilpczjikox.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjZGJkcHFuYnRpbHBjemppa294Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NDU2OTUwNywiZXhwIjoyMDcwMTQ1NTA3fQ.Gji2X_wfrYlCGImiKTX5b3cIEkc36o0sP5jCwASXLfg'
+  }
+};
